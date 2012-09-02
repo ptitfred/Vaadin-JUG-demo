0c32526e5f23179f24bff7e6a0a7938576669408
Nicolas Frankel <nicolas@frankel.ch>
2012-09-02 15:26:03 +0200

    Changed layout from horizontal to vertical

--- jugdemo/src/main/java/com/morevaadin/vaadin7/jugdemo/DemoUI.java
+++ jugdemo/src/main/java/com/morevaadin/vaadin7/jugdemo/DemoUI.java
@@ -3,10 +3,10 @@ package com.morevaadin.vaadin7.jugdemo;
 import com.vaadin.terminal.WrappedRequest;
 import com.vaadin.ui.Button;
 import com.vaadin.ui.Button.ClickEvent;
-import com.vaadin.ui.HorizontalLayout;
 import com.vaadin.ui.Label;
 import com.vaadin.ui.UI;
 import com.vaadin.ui.TextField;
+import com.vaadin.ui.VerticalLayout;
 
 @SuppressWarnings("serial")
 public class DemoUI extends UI {
@@ -18,7 +18,10 @@ public class DemoUI extends UI {
 	@Override
 	protected void init(WrappedRequest request) {
 
-		HorizontalLayout layout = new HorizontalLayout();
+		VerticalLayout layout = new VerticalLayout();
+
+		layout.setMargin(true);
+		layout.setSpacing(true);
 
 		Button button = new Button("Say Hello");
 
