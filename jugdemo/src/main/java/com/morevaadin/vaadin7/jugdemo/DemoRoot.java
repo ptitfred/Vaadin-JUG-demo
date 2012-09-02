0b82ad53701b0817a608d5090dc56aa7bf646e04
Nicolas Frankel <nicolas@frankel.ch>
2012-09-02 16:58:51 +0200

    Added a welcome file, changed Vaadin servlet mapping to a sub-context and set a logout URL

--- jugdemo/src/main/java/com/morevaadin/vaadin7/jugdemo/DemoRoot.java
+++ jugdemo/src/main/java/com/morevaadin/vaadin7/jugdemo/DemoRoot.java
@@ -19,6 +19,8 @@ public class DemoRoot extends Root {
 		display.showView(new LoginView());
 
 		setContent(display);
+
+		Application.getCurrent().setLogoutURL(".");
 	}
 
 	void login(String login, String password) {
