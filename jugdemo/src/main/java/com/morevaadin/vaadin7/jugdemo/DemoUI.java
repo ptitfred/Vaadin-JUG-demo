1ae800288e92c6d1a1716262831035508c45d2c9
Nicolas Frankel <nicolas@frankel.ch>
2012-09-02 15:17:05 +0200

    Changed listener implementation to reflection

--- jugdemo/src/main/java/com/morevaadin/vaadin7/jugdemo/DemoUI.java
+++ jugdemo/src/main/java/com/morevaadin/vaadin7/jugdemo/DemoUI.java
@@ -3,7 +3,6 @@ package com.morevaadin.vaadin7.jugdemo;
 import com.vaadin.terminal.WrappedRequest;
 import com.vaadin.ui.Button;
 import com.vaadin.ui.Button.ClickEvent;
-import com.vaadin.ui.Button.ClickListener;
 import com.vaadin.ui.HorizontalLayout;
 import com.vaadin.ui.Label;
 import com.vaadin.ui.UI;
@@ -12,31 +11,30 @@ import com.vaadin.ui.TextField;
 @SuppressWarnings("serial")
 public class DemoUI extends UI {
 
+	private TextField text = new TextField("", "JUG");
+
+	private Label label = new Label();
+
 	@Override
 	protected void init(WrappedRequest request) {
 
 		HorizontalLayout layout = new HorizontalLayout();
 
-		final TextField text = new TextField("", "JUG");
-
 		Button button = new Button("Say Hello");
 
-		final Label label = new Label();
-
-		button.addListener(new ClickListener() {
-
-			public void buttonClick(ClickEvent event) {
-
-				String value = text.getValue();
-
-				label.setValue("Hello " + value + "!");
-			}
-		});
+		button.addListener(ClickEvent.class, this, "sayHello");
 
 		layout.addComponent(text);
 		layout.addComponent(button);
 		layout.addComponent(label);
 
 		setContent(layout);
+	}
+
+	public void sayHello() {
+
+		String value = text.getValue();
+
+		label.setValue("Hello " + value + "!");
 	}
 }
